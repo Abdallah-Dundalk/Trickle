7c79891ef2503e62bdbc5b88a3a3ff236a6d4793
Abdallah-Dundalk <Abdallah.A.Ahmed@outlook.com>
2023-02-05 16:28:19 +0000

    Change index view so all songs are rendered on index page and style images and song info.

--- home/views.py
+++ home/views.py
@@ -4,10 +4,21 @@ from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
 from . models import Song
 
+# def index(request):
+#     """ A view to return the index page"""
+#     paginator = Paginator(Song.objects.all(), 1)
+#     page_number = request.GET.get('page')
+#     page_obj = paginator.get_page(page_number)
+#     context = {"page_obj": page_obj}
+#     return render(request, 'home/index.html', context)
+
+
 def index(request):
-    """ A view to return the index page"""
-    paginator = Paginator(Song.objects.all(), 1)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    context = {"page_obj": page_obj}
+    """A view to return all songs on index page"""
+    songs = Song.objects.all()
+
+    context = {
+        'songs': songs,
+    }
+
     return render(request, 'home/index.html', context)
