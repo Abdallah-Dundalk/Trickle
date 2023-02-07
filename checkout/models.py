53d21ea7a23b02346cd008b17c38ae44d325572d
Abdallah-Dundalk <Abdallah.A.Ahmed@outlook.com>
2023-02-07 21:35:25 +0000

    migrate 'Order' model.

--- checkout/models.py
+++ checkout/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
@@ -14,4 +16,21 @@ class Order(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
 
+    def generate_order_number(self):
+        """
+        Generate a random, unique order number using UUID
+        """
+        return uuid.uuid4().hex.upper()
 
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number
+        if it hasnt been set already
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+        
