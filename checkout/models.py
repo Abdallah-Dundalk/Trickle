f598e411a58f740e3e8d37d8b2d14cba81b27a1d
Abdallah-Dundalk <Abdallah.A.Ahmed@outlook.com>
2023-02-12 18:12:00 +0000

    Add checkout app and 'Order' model.

---
+++ checkout/models.py
@@ -0,0 +1,35 @@
+import uuid
+from django.db import models
+from django.conf import settings
+
+
+class Order(models.Model):
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    country = models.CharField(max_length=40, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=False, blank=False)
+    date = models.DateField(auto_now_add=True)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+
+    def _generate_order_number(self):
+        """
+        Generte a random, unique order number using UUID
+        """
+        return uuid.uuid4.hex.upper()
+
+    
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number
+        if it hasn't been set already
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
