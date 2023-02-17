60f0352f67047411b3bd6869ce18091aad2c6735
Abdallah-Dundalk <Abdallah.A.Ahmed@outlook.com>
2023-02-17 23:02:22 +0000

    Add loading screen on checkout page.

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -55,6 +55,8 @@ form.addEventListener('submit', function(ev) {
     card.update({ 'disabled': true});
     console.log("card updated")
     $('#submit-button').attr('disabled', true);
+    $('#payment-form').fadeToggle(100)
+    $('#loading-screen').fadeToggle(100)
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
@@ -63,11 +65,13 @@ form.addEventListener('submit', function(ev) {
         if (result.error) {
             var errorDiv = document.getElementById('card-errors');
             var html = `
-                <span class="icon" role="alert">
+                <span class="icon alert-danger" role="alert">
                 <i class="fas fa-times"></i>
                 </span>
-                <span>${result.error.message}</span>`;
+                <span class="icon alert-danger">${result.error.message}</span>`;
             $(errorDiv).html(html);
+            $('#payment-form').fadeToggle(100)
+            $('#loading-screen').fadeToggle(100)
             card.update({ 'disabled': false});
             $('#submit-button').attr('disabled', false);
         } else {
